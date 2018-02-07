5af3efb6636406a275ffdf21c8a95d9d173a3ddc
ryfles2 <rafalpolanin@gmail.com>
2018-02-07 02:22:50 +0100

    fix reserwation/buy button

--- app/src/main/java/com/example/ryfles/theatre/RepertoireFragment.java
+++ app/src/main/java/com/example/ryfles/theatre/RepertoireFragment.java
@@ -12,6 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.ryfles.theatre.Interface.ItemClickListener;
 import com.example.ryfles.theatre.Models.DataModel;
@@ -21,6 +22,8 @@ import com.example.ryfles.theatre.ViewHolder.DataViewHolder;
 import com.example.ryfles.theatre.ViewHolder.RepertoireViewHolder;
 import com.example.ryfles.theatre.ViewHolder.SiteViewHolder;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
@@ -42,7 +45,7 @@ public class RepertoireFragment extends Fragment {
     private RecyclerView recyclerRepertuar;
     private RecyclerView.LayoutManager layoutManager;
     String chooseFilm,chooseData,posterURL,seatId,branchSeat;
-
+    private FirebaseAuth mAuth;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
@@ -56,6 +59,8 @@ public class RepertoireFragment extends Fragment {
         layoutManager = new GridLayoutManager(getContext(),1);
        // layoutManager.layoutDecoratedWithMargins(view.findViewById(R.id.repertuaRecyclerMenu),20,0,0,20);
         recyclerRepertuar.setLayoutManager(layoutManager);
+        mAuth = FirebaseAuth.getInstance();
+
 
         loadMenu();
         return view;
@@ -112,20 +117,31 @@ public class RepertoireFragment extends Fragment {
         final DatabaseReference data = database.getReference("idMiejsce/" + seatId );
         adapterSite = new FirebaseRecyclerAdapter<SiteModel, SiteViewHolder>(SiteModel.class,R.layout.menu_id_data_list,SiteViewHolder.class,data) {
             @Override
-            protected void populateViewHolder(final SiteViewHolder viewHolder, SiteModel model, final int position) {
+            protected void populateViewHolder(final SiteViewHolder viewHolder, final SiteModel model, final int position) {
                 viewHolder.textView.setText(Integer.toString(position+1));
                 final String status = model.getStatus();
-
-                if(model.getStatus().equals("0")) {
+                final String IdKupujacego = model.getIdKupujacego();
+                final FirebaseUser currentUser = mAuth.getCurrentUser();
+                boolean user=false;
+                if (currentUser != null)
+                    user =model.getIdKupujacego().equals(currentUser.getEmail().toString());
+                if( user && model.getStatus().equals("3") ){
+                    viewHolder.textView.setBackgroundColor(Color.BLUE);
+                }
+                else if(model.getStatus().equals("0")) {
                     viewHolder.textView.setBackgroundColor(Color.GREEN);
-                }
-                else if(model.getStatus().equals("1") || model.getStatus().equals("3") ) {
-                    viewHolder.textView.setBackgroundColor(Color.YELLOW);
-                }
-
+                }//|| model.getStatus().equals("3")
+//                else if(model.getStatus().equals("1")   ) {
+//                    viewHolder.textView.setBackgroundColor(Color.YELLOW);
+//                }
                 else if(model.getStatus().equals("2")) {
                     viewHolder.textView.setBackgroundColor(Color.RED);
                 }
+                else {
+                    viewHolder.textView.setBackgroundColor(Color.YELLOW);
+                }
+
+
 
 
                 viewHolder.setItemClickListener(new ItemClickListener() {
@@ -133,16 +149,36 @@ public class RepertoireFragment extends Fragment {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
                         String position1 = Integer.toString(position+1);
-                        if((status.equals("3"))) {
-                            viewHolder.textView.setBackgroundColor(Color.GREEN);
-                            SiteModel model= new SiteModel("2","1");
-                            data.child(position1).setValue(model);
+                        if (currentUser != null)
+                        {
+                            if((status.equals("3")) && IdKupujacego.equals(currentUser.getEmail().toString()) ) {
+                                SiteModel model= new SiteModel("0","0");
+                                data.child(position1).setValue(model);
+                                viewHolder.textView.setBackgroundColor(Color.GREEN);
 
+                            }
+                            else if(status.equals("0")) {
+
+                                SiteModel model= new SiteModel(currentUser.getEmail().toString(),"3");
+                                data.child(position1).setValue(model);
+                                viewHolder.textView.setBackgroundColor(Color.BLUE);
+                            }
                         }
-                        else if(status.equals("0")) {
-                            viewHolder.textView.setBackgroundColor(Color.BLUE);
-                            //data.child(seatId).child(position1).child("Status").setValue("3");
+                        else {
+                            Toast.makeText(getContext(),"Please log in",Toast.LENGTH_SHORT).show();
                         }
+//                        if((status.equals("3"))) {
+//                            SiteModel model= new SiteModel("0","0");
+//                            data.child(position1).setValue(model);
+//                            viewHolder.textView.setBackgroundColor(Color.GREEN);
+//
+//                        }
+//                        else if(status.equals("0")) {
+//
+//                            SiteModel model= new SiteModel(currentUser.getEmail().toString(),"3");
+//                            data.child(position1).setValue(model);
+//                            viewHolder.textView.setBackgroundColor(Color.BLUE);
+//                        }
                     }
                 });
             }
